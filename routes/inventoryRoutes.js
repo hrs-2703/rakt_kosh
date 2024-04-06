b46516a8f3e789c209bace899fa7cf22c12e7932
Harsh Tiwary <itsharshofficial27@gmail.com>
2024-04-06 10:52:08 +0530

    adding inventory route for blood bank

---
+++ routes/inventoryRoutes.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { createInventoryController, getInventoryController, getDonarsController, getHospitalController, getOrgnaisationController, getOrgnaisationForHospitalController, getInventoryHospitalController, getRecentInventoryController } = require("../controllers/inventoryController");
+const authmiddleware = require("../middlewares/authmiddleware");
+
+
+
+const router = express.Router();
+
+//routes
+// ADD INVENTORY || POST
+router.post("/create-inventory", authmiddleware, createInventoryController);
+
+router.get("/get-inventory", authmiddleware, getInventoryController);
+
+//GET DONAR RECORDS
+router.get("/get-donars",  authmiddleware, getDonarsController);
+
+//GET HOSPITAL RECORDS
+router.get("/get-hospitals", authmiddleware, getHospitalController);
+
+//GET orgnaisation RECORDS
+router.get("/get-orgnaisation", authmiddleware, getOrgnaisationController);
+
+router.get(
+  "/get-orgnaisation-for-hospital",
+ authmiddleware,
+  getOrgnaisationForHospitalController
+);
+//GET RECENT BLOOD RECORDS
+router.get(
+  "/get-recent-inventory",
+ authmiddleware,
+  getRecentInventoryController
+);
+
+//GET HOSPITAL BLOOD RECORDS
+router.post(
+  "/get-inventory-hospital",
+ authmiddleware,
+  getInventoryHospitalController
+);
+module.exports = router;
